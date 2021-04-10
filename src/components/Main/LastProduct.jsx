49686d602f591f9817584d748200d5e806dc4e8c
Agustina Sánchez Ré <agustinasanchezre@gmail.com>
2021-04-10 13:00:31 -0300

    adding little changes to last detail panel

--- src/components/Main/LastProduct.jsx
+++ src/components/Main/LastProduct.jsx
@@ -6,7 +6,9 @@ const LastProduct = ({lastProduct}) => {
 			<div className="text-center">
 				<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width:"25rem"}} src={lastProduct.main_image} alt="prod_img" />
 			</div>
-			<h4>{lastProduct.title}</h4><br/><p>{lastProduct.product_detail}</p>
+			<h5>{lastProduct.title}</h5>
+			<p>{lastProduct.summary}</p>
+			<a target="_blank" rel="nofollow" href="http://localhost:3001/api/products/last">Visualizar la información completa del producto</a>
 		</div>
     );
 }
